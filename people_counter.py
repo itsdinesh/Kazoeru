068d67379a484ce4e22c6a814ddf07fbb77a9b9a
Dinesh <apiit.dinesh@gmail.com>
2021-06-21 08:20:59 +0800

    add: live counter, video length meter, frame skips

--- people_counter.py
+++ people_counter.py
@@ -7,70 +7,36 @@ from centroid_tracker import CentroidTracker
 protopath = "models/MobileNetSSD_deploy.prototxt"
 modelpath = "models/MobileNetSSD_deploy.caffemodel"
 
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+           "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+           "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+           "sofa", "train", "tvmonitor"]
+
 detector = cv2.dnn.readNetFromCaffe(prototxt=protopath, caffeModel=modelpath)
-tracker = CentroidTracker(maxDisappeared=40)
-
-
-def non_max_suppression_fast(boxes, overlapThresh):
-    try:
-        if len(boxes) == 0:
-            return []
-
-        if boxes.dtype.kind == "i":
-            boxes = boxes.astype("float")
-
-        pick = []
-
-        x1 = boxes[:, 0]
-        y1 = boxes[:, 1]
-        x2 = boxes[:, 2]
-        y2 = boxes[:, 3]
-
-        area = (x2 - x1 + 1) * (y2 - y1 + 1)
-        idxs = np.argsort(y2)
-
-        while len(idxs) > 0:
-            last = len(idxs) - 1
-            i = idxs[last]
-            pick.append(i)
-
-            xx1 = np.maximum(x1[i], x1[idxs[:last]])
-            yy1 = np.maximum(y1[i], y1[idxs[:last]])
-            xx2 = np.minimum(x2[i], x2[idxs[:last]])
-            yy2 = np.minimum(y2[i], y2[idxs[:last]])
-
-            w = np.maximum(0, xx2 - xx1 + 1)
-            h = np.maximum(0, yy2 - yy1 + 1)
-
-            overlap = (w * h) / area[idxs[:last]]
-
-            idxs = np.delete(idxs, np.concatenate(([last],
-                                                   np.where(overlap > overlapThresh)[0])))
-
-        return boxes[pick].astype("int")
-    except Exception as e:
-        print("Exception occurred in non_max_suppression : {}".format(e))
-
+tracker = CentroidTracker(maxDisappeared=15)
+fps_start_time = datetime.datetime.now()
 
 def main():
     cap = cv2.VideoCapture('vid/LRT Pasar Seni.mp4')
-
-    fps_start_time = datetime.datetime.now()
     total_frames = 0
+    object_id_list = []
+    frameCount = 0
 
     while True:
+        # Capture every 15th Frame
+        frameCount += 15
+        cap.set(1, frameCount)
+
         ret, frame = cap.read()
         frame = imutils.resize(frame, width=800)
-        total_frames = total_frames + 1
 
         # If the last frame is reached, reset the capture and the frame_counter
-        if total_frames == cap.get(cv2.CAP_PROP_FRAME_COUNT):
-            total_frames = 0  # Or whatever as long as it is the same as next line
+        if frameCount == cap.get(cv2.CAP_PROP_FRAME_COUNT) - frameCount:
+            frameCount = 0
             cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
         (H, W) = frame.shape[:2]
-
-        blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
+        blob = cv2.dnn.blobFromImage(frame, 0.007843, (W,H), 127.5)
 
         detector.setInput(blob)
         person_detections = detector.forward()
@@ -78,43 +44,46 @@ def main():
 
         for i in np.arange(0, person_detections.shape[2]):
             confidence = person_detections[0, 0, i, 2]
-            if confidence > 0.2:
-                # idx = int(person_detections[0, 0, i, 1])
+            if confidence > 0.5:
+                idx = int(person_detections[0, 0, i, 1])
+
+                if CLASSES[idx] != "person":
+                     continue
+
                 person_box = person_detections[0, 0, i, 3:7] * np.array([W, H, W, H])
-               #  (startX, startY, endX, endY) = person_box.astype("int")
+                (startX, startY, endX, endY) = person_box.astype("int")
                 rects.append(person_box)
 
         boundingboxes = np.array(rects)
-        boundingboxes = boundingboxes.astype(int)
-        rects = non_max_suppression_fast(boundingboxes, 0.3)
-        centroid_dict = dict()
+        boundingboxes.astype(int)
         objects = tracker.update(rects)
+
         for (objectId, bbox) in objects.items():
             x1, y1, x2, y2 = bbox
             x1 = int(x1)
             y1 = int(y1)
             x2 = int(x2)
             y2 = int(y2)
-            cX = int((x1 + x2) / 2.0)
-            cY = int((y1 + y2) / 2.0)
 
-            centroid_dict[objectId] = (cX, cY, x1, y1, x2, y2)
-            text = "ID: {}".format(objectId)
-            cv2.putText(frame, text, (x1, y1 - 5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
+            #text = "ID: {}".format(objectId)
+            #cv2.putText(frame, text, (x1, y1 - 5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
 
-        for id, box in centroid_dict.items():
-            cv2.rectangle(frame, (box[2], box[3]), (box[4], box[5]), (0, 255, 0), 2)
+            #if objectId not in object_id_list:
+            #    object_id_list.append(objectId)
 
-        fps_end_time = datetime.datetime.now()
-        time_diff = fps_end_time - fps_start_time
-        if time_diff.seconds == 0:
-            fps = 0.0
-        else:
-            fps = (total_frames / time_diff.seconds)
+        lpc_count = len(objects)
+        #opc_count = len(object_id_list)
 
-        fps_text = "FPS: {:.2f}".format(fps)
+        lpc_txt = "Live Person Count: {}".format(lpc_count)
+        #opc_txt = "OPC: {}".format(opc_count)
 
-        cv2.putText(frame, fps_text, (5, 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
+        cv2.putText(frame, lpc_txt, (5, 30), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 255), 1)
+        #cv2.putText(frame, opc_txt, (5, 90), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
+
+        length = frameCount/30
+        length = "Video Length: {:.2f}".format(length)
+        cv2.putText(frame, length, (5, 60), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 255), 1)
 
         cv2.imshow("Application", frame)
         key = cv2.waitKey(1)
@@ -122,6 +91,5 @@ def main():
             break
 
     cv2.destroyAllWindows()
-
 
 main()
