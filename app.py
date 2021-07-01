a6bc1e5d7f79d7975c684a1fe2b332a79d836dd7
Dinesh <apiit.dinesh@gmail.com>
2021-07-01 10:36:29 +0800

    add: port to Flask

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, Response
+from people_counter import Camera
+from waitress import serve
+
+app = Flask(__name__)
+
+
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/')
+def test_feed():
+    return render_template('video.html')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run()
--- people_counter.py
+++ people_counter.py
@@ -3,6 +3,7 @@ import datetime
 import imutils
 import numpy as np
 
+from base_camera import BaseCamera
 from centroid_tracker import CentroidTracker
 protopath = "models/MobileNetSSD_deploy.prototxt"
 modelpath = "models/MobileNetSSD_deploy.caffemodel"
@@ -17,95 +18,127 @@ detector = cv2.dnn.readNetFromCaffe(prototxt=protopath, caffeModel=modelpath)
 tracker = CentroidTracker(maxDisappeared=10)
 fps_start_time = datetime.datetime.now()
 
+class Camera(BaseCamera):
 
-def main():
-    cap = cv2.VideoCapture('vid/LRT Encoded V8.3.mkv')
-    total_frames = 0
-    object_id_list = []
-    frameCount = 0
-    frameRate = 10
-    status = "N/A"
+    video_source = "vid/LRT Encoded v6.1.mkv"
 
-    while True:
-        # Capture every 10th frame of the footage
-        frameCount += frameRate
-        cap.set(1, frameCount)
+    @staticmethod
+    def set_video_source(source):
+        Camera.video_source = source
 
-        ret, frame = cap.read()
-        frame = imutils.resize(frame, width=800)
+    @staticmethod
+    def frames():
+        frame_counter = 0
+        cap = cv2.VideoCapture(Camera.video_source)
+        total_frames = 0
+        object_id_list = []
+        frameCount = 0
+        frameRate = 10
+        status = "N/A"
 
-        # If the last frame is almost reached, restart the footage from the first frame.
-        if (frameCount + frameRate) > cap.get(cv2.CAP_PROP_FRAME_COUNT):
-            frameCount = 0
-            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        if not cap.isOpened():
+            raise RuntimeError('Could not start camera.')
 
-        (H, W) = frame.shape[:2]
-        blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
+        while True:
+            # Capture every 10th frame of the footage
+            frameCount += frameRate
+            cap.set(1, frameCount)
 
-        detector.setInput(blob)
-        person_detections = detector.forward()
-        rects = []
+            ret, frame = cap.read()
+            frame = imutils.resize(frame, width=800)
 
-        for i in np.arange(0, person_detections.shape[2]):
-            confidence = person_detections[0, 0, i, 2]
-            if confidence > 0.75:
-                idx = int(person_detections[0, 0, i, 1])
+            # If the last frame is almost reached, restart the footage from the first frame.
+            if (frameCount + frameRate) > cap.get(cv2.CAP_PROP_FRAME_COUNT):
+                frameCount = 0
+                cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
-                if CLASSES[idx] == "train":
-                    print("Train")
+            (H, W) = frame.shape[:2]
+            blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
 
-                if CLASSES[idx] != "person":
-                    continue
+            detector.setInput(blob)
+            person_detections = detector.forward()
+            rects = []
 
-                person_box = person_detections[0, 0, i, 3:7] * np.array([W, H, W, H])
-                (startX, startY, endX, endY) = person_box.astype("int")
-                rects.append(person_box)
-                label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
-                cv2.putText(frame, label, (startX, startY), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
+            for i in np.arange(0, person_detections.shape[2]):
+                confidence = person_detections[0, 0, i, 2]
+                if confidence > 0.75:
+                    idx = int(person_detections[0, 0, i, 1])
 
-        boundingboxes = np.array(rects)
-        boundingboxes.astype(int)
-        objects = tracker.update(rects)
+                    if CLASSES[idx] == "train":
+                        print("Train")
 
-        for (objectId, bbox) in objects.items():
-            x1, y1, x2, y2 = bbox
-            x1 = int(x1)
-            y1 = int(y1)
-            x2 = int(x2)
-            y2 = int(y2)
+                    if CLASSES[idx] != "person":
+                        continue
 
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
-            # text = "ID: {}".format(objectId)
-            # cv2.putText(frame, text, (x1, y1 - 5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
+                    person_box = person_detections[0, 0, i, 3:7] * np.array([W, H, W, H])
+                    (startX, startY, endX, endY) = person_box.astype("int")
+                    rects.append(person_box)
+                    label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+                    cv2.putText(frame, label, (startX, startY), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
 
-            # if objectId not in object_id_list:
-            #    object_id_list.append(objectId)
+            boundingboxes = np.array(rects)
+            boundingboxes.astype(int)
+            objects = tracker.update(rects)
 
-        # Person Count Indicator
-        lpc_count = len(objects)
-        lpc_txt = "Live Person Count: {}".format(lpc_count)
-        cv2.putText(frame, lpc_txt, (5, 30), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 255), 1)
+            for (objectId, bbox) in objects.items():
+                x1, y1, x2, y2 = bbox
+                x1 = int(x1)
+                y1 = int(y1)
+                x2 = int(x2)
+                y2 = int(y2)
 
-        # Video Length Indicator
-        length = frameCount / 30
-        length = "Video Length: {:.2f}".format(length)
-        cv2.putText(frame, length, (5, 60), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 255), 1)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
+                # text = "ID: {}".format(objectId)
+                # cv2.putText(frame, text, (x1, y1 - 5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
 
-        # Crowd Status Indicator
-        if (lpc_count) > 5:
-            status = "Crowded"
-            cv2.putText(frame, status, (5, 90), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
-        else:
-            status = "Not Crowded"
-            cv2.putText(frame, status, (5, 90), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 128, 0), 2)
+                # if objectId not in object_id_list:
+                #    object_id_list.append(objectId)
 
-        # OpenCV Window
-        cv2.imshow("Application", frame)
-        key = cv2.waitKey(1)
-        if key == ord('q'):
-            break
+            # Person Count Indicator
+            lpc_count = len(objects)
+            lpc_txt = "Live Person Count: {}".format(lpc_count)
+            cv2.putText(frame, lpc_txt, (5, 30), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 255), 1)
 
-    cv2.destroyAllWindows()
+            # Video Length Indicator
+            length = frameCount / 30
+            length = "Video Length: {:.2f}".format(length)
+            cv2.putText(frame, length, (5, 60), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 255), 1)
+
+            # Crowd Status Indicator
+            if (lpc_count) > 5:
+                status = "Crowded"
+                cv2.putText(frame, status, (5, 90), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
+            else:
+                status = "Not Crowded"
+                cv2.putText(frame, status, (5, 90), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 128, 0), 2)
+
+            # Pass as JPG frames
+            yield cv2.imencode('.jpg', frame)[1].tobytes()
+
+            # # OpenCV Window
+            # cv2.imshow("Application", frame)
+            # key = cv2.waitKey(1)
+            # if key == ord('q'):
+            #     break
+
+    @staticmethod
+    def frames2():
+        frame_counter = 0
+        camera = cv2.VideoCapture(Camera.video_source)
+        if not camera.isOpened():
+            raise RuntimeError('Could not start camera.')
+
+        while True:
+            frame_counter += 1
+            # read current frame
+            _, img = camera.read()
+
+            #if we reach the end of video file
+            if frame_counter == camera.get(cv2.CAP_PROP_FRAME_COUNT):
+                frame_counter = 0
+                camera = cv2.VideoCapture(Camera.video_source)
+
+            # encode as a jpeg image and return it
+            yield cv2.imencode('.jpg', img)[1].tobytes()
 
 
-main()
