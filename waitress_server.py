ffae333661fba7b8cbefa9250c7d8f5894cc3c17
Dinesh <apiit.dinesh@gmail.com>
2021-07-10 08:48:12 +0800

    change: rename main app to app2

--- waitress_server.py
+++ waitress_server.py
@@ -1,5 +1,5 @@
 from waitress import serve
-import app
+import app2
 print("Waitress WSGI Server is running!")
-serve(app.app, channel_timeout=4000, host='0.0.0.0', port=8080)
+serve(app2.app, channel_timeout=4000, host='0.0.0.0', port=8080)
 
