b3172cd688bdbd24d04ab26e73b187b226038256
Dinesh <apiit.dinesh@gmail.com>
2021-07-11 08:28:33 +0800

    add: design tweaks & print waitress link

--- waitress_server.py
+++ waitress_server.py
@@ -1,5 +1,6 @@
 from waitress import serve
 import app
 print("Waitress WSGI Server is running!")
+print("Running on http://127.0.0.1:8080")
 serve(app.application, channel_timeout=4000, host='0.0.0.0', port=8080, threads=6)
 
